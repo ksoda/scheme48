b260026cf931f2cdc57b4e63f87193ee8a2ebf80
Ken Sonoda <ken.soda0@gmail.com>
2016-10-09 01:20:43 +0900

    :white_check_mark: Add specs

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,5 @@ import Lib
 main :: IO ()
 main = do
     args <- getArgs
-    evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
+    evaled <- return $ liftM show $ readExpr (head args) >>= eval
     putStrLn $ extractValue $ trapError evaled
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,23 +1,28 @@
 import Test.HUnit
 import Lib
+import Data.Either
 
 main :: IO ()
 main = do
   runTestTT tests
   return ()
 
-readEvalShow = show . eval . readExpr
+readEvalShow x = do
+    evaled <- return $ liftM show $ readExpr x >>= eval
+    extractValue $ trapError evaled
+readEval x = readExpr x >>= eval
+readShow = show . readExpr
 tests = TestList
-  [ "string" ~: readEvalShow "\"this is a string\"" ~?= "\"this is a string\""
-  , "number" ~: readEvalShow "25"                   ~?= "25"
-  , "list" ~: readEvalShow "(symbol)"               ~?= "#f"
-  , "list" ~: readEvalShow "(a test)"               ~?= "#f"
-  , "list" ~: readEvalShow "(a (nested) test)"                  ~?= "#f"
-  , "list" ~: readEvalShow "(a (dotted . list) test)"           ~?= "#f"
-  , "list" ~: readEvalShow "(a '(quoted (dotted . list)) test)" ~?= "#f"
-  , "list" ~: take 9 (readEvalShow "(a '(imbalanced parens)") ~?= "\"No match"
-  , "list" ~: readEvalShow "(+ 2 2)" ~?= "4"
-  , "list" ~: readEvalShow "(+ 2 (-4 1))" ~?= "2"
-  , "list" ~: readEvalShow "(+ 2 (- 4 1))" ~?= "5"
-  , "list" ~: readEvalShow "(- (+ 4 6 3) 3 5 2)" ~?= "3"
+  [ "read number" ~: readShow "42"                   ~?= "Right 42"
+  , "read string" ~: readShow "\"this is a string\"" ~?= "Right \"this is a string\""
+  , "read nested list" ~: readShow "(a (nested . dot) '(test))"
+    ~?= "Right (a (nested . dot) '(test))"
+  , "read string" ~: (isLeft . return . readExpr) "(a '(imbalanced parens)" ~?= True
+  , "list" ~: (show . readEval $ "(+ 2 (+ 1 1 1))") ~?= "Right 5"
+  , "list" ~: (show . readEval $ "(-4 1)")
+    ~?= "Left Unrecognized primitive function args: \"-4\""
+  , "list" ~: readEvalShow "(+ 1 1)" ~?= "2"
+  , "list" ~: readEvalShow "(+ 1)" ~?= "Expected 2 args; found values 1"
+  , "list" ~: readEvalShow "(+ 2 \"two\")" ~?= "Invalid type: expected number, found \"two\""
+  , "list" ~: readEvalShow "(what? 2)" ~?= "Unrecognized primitive function args: \"what?\""
   ]
